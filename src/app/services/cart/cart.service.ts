8a673b28da56ee6b15b34df778e89b56dfbd05a0
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-11-07 00:17:52 +0100

    end of frontend

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -7,12 +7,13 @@ import { Food } from 'src/app/shared/models/Food';
   providedIn: 'root'
 })
 export class CartService {
-private cart:Cart=new Cart();
+private cart:Cart=this.getCartFromLocalStorage();
 
 addToCart(food:Food):void{
   let cartItem= this.cart.items.find(item=>item.food.id==food.id);
 if(cartItem){
   this.changeQuantity(food.id,cartItem.quantity+1);
+  this.setCartToLocalStorage();
   return;
 }
 this.cart.items.push(new CartItem(food));
@@ -21,15 +22,24 @@ this.cart.items.push(new CartItem(food));
 removeFromCart(foodId:number):void{
   this.cart.items=
   this.cart.items.filter(item=>item.food.id!=foodId);
+  this.setCartToLocalStorage();
 }
 
 changeQuantity(foodId:number, quantity:number){
 let cartItem =this.cart.items.find(item=>item.food.id===foodId);
 if(!cartItem) return;
 cartItem.quantity=quantity;
+this.setCartToLocalStorage();
 }
 getCart():Cart{
   return this.cart
 }
-
+private setCartToLocalStorage():void{
+  const cartJson = JSON.stringify(this.cart);
+  localStorage.setItem('Cart', cartJson)
+}
+private getCartFromLocalStorage(): Cart {
+  const cartJson = localStorage.getItem('Cart');
+  return cartJson ? JSON.parse(cartJson) : new Cart();
+}
 }
